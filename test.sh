7e7dd86bce90c6d8ac0b5047b8630f9fea9f25fd
Yuxi Liu <liu.yuxi.canaan@gmail.com>
2024-11-17 18:28:08 +0800

    重新整理 install 和 uninstall 命令

--- test.sh
+++ test.sh
@@ -7,6 +7,17 @@ sudo rm -f "/usr/local/sbin/yxct"
 # additional_params="--info-level verbose --test"
 additional_params="--info-level verbose"
 
+# sudo which yxct
+
+# if ! sudo which yxct; then
+#   echo "not exist"
+# fi
+
+# exit 0
+
+function check_yxct_not_exist() { if sudo which yxct > /dev/null; then (>&2 echo "$(tput setaf 1)Fatal!!!!$(tput sgr0)"); fi; }
+function check_yxct_exist() { if ! sudo which yxct > /dev/null; then (>&2 echo "$(tput setaf 1)Fatal!!!!$(tput sgr0)"); fi; }
+
 
 # sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/.yxct.setup.py" ${additional_params}
 # echo '===================================================================================================='
@@ -19,9 +30,9 @@ additional_params="--info-level verbose"
 
 # yxct uninstall xxx
 
-sudo ./yxct install yxct --strict --usr-local-sbin --path "${TestPath}/yxct" --test ${additional_params}
+# sudo ./yxct install yxct --strict --usr-local-sbin --path "${TestPath}/yxct" --test ${additional_params}
 
-exit 0
+# exit 0
 
 
 echo "####################################################################################################"
@@ -29,18 +40,35 @@ echo "# Test install command"
 echo "####################################################################################################"
 
 echo "如果提供了脚本路径 -> 脚本路径是一个文件 -> 文件是安装脚本, 直接调用脚本"
+check_yxct_not_exist
 sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/.yxct.setup.sh" ${additional_params}
+check_yxct_exist
 sudo ./yxct uninstall yxct
+check_yxct_not_exist
 echo '===================================================================================================='
 
 echo "如果提供了脚本路径 -> 脚本路径是一个文件 -> 文件不是安装脚本, 则直接安装文件"
+check_yxct_not_exist
 sudo ./yxct install yxct --force --usr-local-sbin --path "${TestPath}/yxct" ${additional_params}
+check_yxct_exist
 sudo ./yxct uninstall yxct
+check_yxct_not_exist
 echo '===================================================================================================='
 
 echo "如果提供了脚本路径 -> 脚本路径是一个目录 -> 目录中找到安装脚本, 直接掉用那个安装脚本"
+check_yxct_not_exist
 sudo ./yxct install yxct --force --usr-local-sbin --path "${TestPath}" ${additional_params}
+check_yxct_exist
 sudo ./yxct uninstall yxct
+check_yxct_not_exist
+echo '===================================================================================================='
+
+echo "测试简单命令"
+check_yxct_not_exist
+sudo ./yxct install yxct --path "${TestPath}"  ${additional_params}
+check_yxct_exist
+sudo ./yxct uninstall yxct ${additional_params}
+check_yxct_not_exist
 echo '===================================================================================================='
 
 # 如果脚本路径是一个目录, 没找到那个安装脚本，查看是否是单脚本APP
@@ -53,49 +81,86 @@ echo ""
 echo ""
 echo ""
 
-
 echo "####################################################################################################"
 echo "# Test uninstall command"
 echo "####################################################################################################"
 
 
 echo "如果提供了脚本路径 -> 如果脚本路径是一个文件 -> 如果文件是一个安装脚本, 直接调用脚本"
+check_yxct_not_exist
 sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/yxct"
+check_yxct_exist
 sudo ./yxct uninstall yxct --path "${TestPath}/.yxct.setup.sh" ${additional_params}
+check_yxct_not_exist
 echo '===================================================================================================='
 
 echo "如果提供了脚本路径 -> 如果脚本路径是一个文件 -> 如果文件不上是安装脚本，尝试查找文件相同的命令, 并删除"
+check_yxct_not_exist
 sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/yxct"
+check_yxct_exist
 sudo ./yxct uninstall yxct --path "${TestPath}/yxct" ${additional_params}
+check_yxct_not_exist
 echo '===================================================================================================='
 
 echo "如果提供了脚本路径 -> 如果脚本径是一个目录 -> 如果目录中找到安装脚本, 直接调用那个安装脚本"
+check_yxct_not_exist
 sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/yxct"
+check_yxct_exist
 sudo ./yxct uninstall yxct --path "${TestPath}" ${additional_params}
+check_yxct_not_exist
 echo '===================================================================================================='
+
 
 # 如果提供了脚本路径 -> 如果脚本路径是一个目录 -> 没找到那个安装脚本，查看是否是单脚本APP
 # 测试是要先移除 .yxct.setup.sh
 #sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}"
 #sudo ./yxct uninstall yxct --path "${TestPath}" --ignore 'bin'
 
-echo "如果没提供脚本路径， 但bin_path不为空 -> 如果bin_path 是个文件, 直接删除， command 提供验证"
+echo "如果没提供脚本路径， 但path不为空 -> 如果path 是个文件, 直接删除， command 提供验证"
 sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/yxct"
+check_yxct_exist
 sudo ./yxct uninstall yxct --custom-dest "/usr/local/sbin/yxct" ${additional_params}
+check_yxct_not_exist
 echo '===================================================================================================='
 
-echo "如果没提供脚本路径， 但bin_path不为空 -> 如果bin_path 是个目录, 则command 不能为空, 根据command删除"
+echo "如果没提供脚本路径， 但path不为空 -> 如果path 是个目录, 则command 不能为空, 根据command删除"
 sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/yxct"
+check_yxct_exist
 sudo ./yxct uninstall yxct --usr-local-sbin ${additional_params}
+check_yxct_not_exist
 echo '===================================================================================================='
 
-echo "如果没提供脚本路径和bin_path, 但提供了command"
+echo "如果没提供脚本路径和path, 但提供了command"
 sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/yxct"
-sudo ./yxct uninstall yxct ${additional_params}
+check_yxct_exist
+sudo ./yxct uninstall yxct ${additional_params} --all
+check_yxct_not_exist
 echo '===================================================================================================='
+
+echo "测试安装脚本，删除多此"
+sudo ./yxct install yxct --usr-local-bin --path "${TestPath}/yxct"
+sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/yxct"
+sudo ./yxct install yxct --usr-bin --path "${TestPath}/yxct"
+sudo ./yxct install yxct --usr-sbin --path "${TestPath}/yxct"
+check_yxct_exist
+sudo ./yxct uninstall yxct --path "${TestPath}" --all ${additional_params} 
+check_yxct_not_exist
+echo '===================================================================================================='
+
+echo "测试无脚本，删除多次"
+sudo ./yxct install yxct --usr-local-bin --path "${TestPath}/yxct"
+sudo ./yxct install yxct --usr-local-sbin --path "${TestPath}/yxct"
+sudo ./yxct install yxct --usr-bin --path "${TestPath}/yxct"
+sudo ./yxct install yxct --usr-sbin --path "${TestPath}/yxct"
+check_yxct_exist
+sudo ./yxct uninstall yxct --all ${additional_params} 
+check_yxct_not_exist
+echo '===================================================================================================='
+
 
 
 echo ""
 echo ""
 echo '===================================================================================================='
 sudo which yxct
+
