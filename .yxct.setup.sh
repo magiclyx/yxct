3426c4d618d724f6e28553d19f0e3f620bcb98a3
Yuxi Liu <liu.yuxi.canaan@gmail.com>
2024-11-28 02:18:04 +0800

    yxct 开始支持 library.

--- .yxct.setup.sh
+++ .yxct.setup.sh
@@ -1,94 +1,83 @@
 #!/bin/bash
 
-# SOURCE="$0"
-# while [ -h "$SOURCE"  ]; do
-#   SRC_DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
-#   SOURCE="$(readlink "$SOURCE")"
-#   [[ $SOURCE != /*  ]] && SOURCE="$SRC_DIR/$SOURCE"
-# done
-# SRC_DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
 
-function echo_fatal()
+
+SCRIPT_NAME=yxct
+
+
+
+####################################################################################################
+# utility
+####################################################################################################
+
+
+function _yxct_is_osx()
+{
+  local _is_osx=false
+
+  if [ -n "${OSTYPE}" ]; then
+    if [[ "${OSTYPE}" == "darwin"* ]]; then 
+      _is_osx=true
+    else
+      _is_osx=false
+    fi
+  else
+    if [[ $(uname | tr '[:upper:]' '[:lower:]') == "darwin"* ]]; then
+      _is_osx=true
+    else
+      _is_osx=false
+    fi
+  fi
+
+  ${_is_osx} && return 0 || return 1
+}
+
+function _yxct_get_library_path()
+{
+  if _yxct_is_osx; then
+    echo "/usr/local/yuxi/${SCRIPT_NAME}"
+  else
+    echo "/usr/yuxi/${SCRIPT_NAME}"
+  fi
+}
+
+
+function yxct_fatal()
 {
   echo "Fatal: $@"
   exit 1
 }
 
-SCRIPT_NAME=yxct
+function yxct_verb()
+{
+  echo $@
+}
+
+function yxct_err()
+{
+  (>&2 echo "$(tput setaf 5)${1}$(tput sgr0)")
+}
+
+function yxct_verbcmd()
+{
+  yxct_verb "$*"
+  $@
+}
+
+function yxct_msg()
+{
+  echo $*
+}
+
+####################################################################################################
+# command
+####################################################################################################
 
 function cmd_install()
 {
-  # if [ -z "$*" ]; then
-  #   echo "cmd_install: Empty"
-  # else
-  #   echo "cmd_install: $*"
-  # fi
-
-    local script_path=$(pwd)
-    local bin_path=
-    local command=
-    local nono=
-
-    while [ $# -gt 0 ]; do
-		case $1 in
-
-      --bin-path )
-        shift
-        bin_path=$1
-      ;;
-
-      --command )
-        shift
-        command=$1
-      ;;
-
-      --nono )
-        shift
-        nono=$1
-      ;;
-
-      *)
-        echo_fatal "Unknown params $1. Use '${CMD} info --help' to show help information"
-      ;;
-    esac
-    shift
-  done
-
-
-  if [[ -z "${command}" ]]; then
-    echo_fatal "Command is empty"
-  fi
-
-  if [ -z "${bin_path}" ]; then
-    echo_fatal "bin path is empty"
-  fi
-
-  if [ -z "${nono}" ]; then
-    echo_fatal "nono is not here"
-  fi
-
-  if [[ "${nono}" != 'abc' ]]; then
-    echo_fatal "Invalid nono: ${nono}"
-  fi
-
-  if [[ "${command}" != "${SCRIPT_NAME}" ]]; then
-    echo_fatal "Invalid script:${command}"
-  fi
-
-  sudo cp -f "${script_path%/}/${SCRIPT_NAME}" "${bin_path%/}"
-  chmod +x "${bin_path%/}/${SCRIPT_NAME}"
-}
-
-function cmd_uninstall()
-{
-  # if [ -z "$*" ]; then
-  #   echo "cmd_uninstall: Empty"
-  # else
-  #   echo "cmd_uninstall: $*"
-  # fi
-
-  local command=
+  local script_path=$(pwd)
   local bin_path=
+  local command=
   local nono=
 
   while [ $# -gt 0 ]; do
@@ -104,13 +93,8 @@ function cmd_uninstall()
         command=$1
       ;;
 
-      --nono )
-        shift
-        nono=$1
-      ;;
-
       *)
-        echo_fatal "Unknown params $1. Use '${CMD} info --help' to show help information"
+        yxct_fatal "Unknown params $1. Use '${CMD} info --help' to show help information"
       ;;
     esac
     shift
@@ -118,43 +102,150 @@ function cmd_uninstall()
 
 
   if [[ -z "${command}" ]]; then
-    echo_fatal "Command is empty"
+    yxct_fatal "Command is empty"
   fi
-
-  if [ -z "${bin_path}" ]; then
-    echo_fatal "bin path is empty"
-  fi
-
-  if [ -z "${nono}" ]; then
-    echo_fatal "nono is not here"
-  fi
-
-  if [[ "${nono}" != 'abc' ]]; then
-    echo_fatal "Invalid nono: ${nono}"
-  fi
-
   if [[ "${command}" != "${SCRIPT_NAME}" ]]; then
-    echo_fatal "Invalid script:${command}"
+    yxct_fatal "Unknown command ${SCRIPT_NAME}"
   fi
 
   if [ -z "${bin_path}" ]; then
-    local app_path=$(which "${SCRIPT_NAME}")
-    if [ -n "${app_path}" ]; then
-      bin_path=$(dirname "${app_path}")
+    yxct_fatal "bin path is empty"
+  fi
+  if ! [[ -d "${bin_path}" ]]; then
+    yxct_fatal "target directory not exist:${bin_path}"
+  fi
+
+
+
+  # verify the pwd
+  if ! [ -f "${script_path}/.yxct.setup.sh" ]; then
+    yxct_fatal "Current working directory not correct:${script_path}"
+  fi
+
+  # copy ${SCRIPT_NAME} to bin path.
+  if ! yxct_verbcmd "cp -f ${script_path%/}/${SCRIPT_NAME} ${bin_path%/}"; then
+    yxct_fatal "failed to copy ${SCRIPT_NAME} to ${SCRIPT_NAME}"
+  fi
+  if ! yxct_verbcmd "chmod +x ${bin_path%/}/${SCRIPT_NAME}"; then
+    yxct_fatal "failed to change yxct's authorization"
+  fi
+
+
+  # make library path
+  local library_path=$(_yxct_get_library_path)
+  if [ -z "${library_path}" ]; then
+    yxct_fatal "failed to get library path"
+  fi
+
+  local library_dir=$(dirname "${library_path}")
+  if ! [ -d "${library_dir}" ]; then
+    yxct_verbcmd "mkdir -p ${library_dir}"
+    if ! [ -d "${library_dir}" ]; then
+      yxct_fatal "Failed to make dir:${library_dir}"
     fi
   fi
 
 
+  # copy index path to library
+  if ! yxct_verbcmd "cp -R ${script_path}/index ${library_path}"; then
+    yxct_fatal "failed to copy index path to ${library_path}"
+  fi
+
+  if ! yxct_verbcmd "chmod -R 755 ${script_path}"; then
+    yxct_fatal "faild to change ${SCRIPT_NAME}'s authorization"
+  fi
+
+
+  yxct_msg "Install success. You need to logout and re-login to reload all environment."
+
+}
+
+function cmd_uninstall()
+{
+  local script_path=$(pwd)
+  local command=
+  local bin_path=
+
+  while [ $# -gt 0 ]; do
+		case $1 in
+
+      --bin-path )
+        shift
+        bin_path=$1
+      ;;
+
+      --command )
+        shift
+        command=$1
+      ;;
+
+      *)
+        yxct_fatal "Unknown params $1. Use '${CMD} info --help' to show help information"
+      ;;
+    esac
+    shift
+  done
+
+
+  if [[ -z "${command}" ]]; then
+    yxct_fatal "Command is empty"
+  fi
+  if [[ "${command}" != "${SCRIPT_NAME}" ]]; then
+    yxct_fatal "Unknown command ${SCRIPT_NAME}"
+  fi
+
+  if [ -z "${bin_path}" ]; then
+    yxct_fatal "bin path is empty"
+  fi
+  if ! [[ -d "${bin_path}" ]]; then
+    yxct_fatal "target directory not exist:${bin_path}"
+  fi
+
+
+  # verify the pwd
+  if ! [ -f "${script_path}/.yxct.setup.sh" ]; then
+    yxct_fatal "Current working directory not correct:${script_path}"
+  fi
+
+
+  local has_err=false
+
+  # remove ${SCRIPT_NAME} from bin-path
   local execute_file="${bin_path}/${command}"
   if [ -x "${execute_file}" ]; then
-    sudo rm -f "${execute_file}" > /dev/null
+    
+    if ! yxct_verbcmd "rm -f ${execute_file}"; then
+      yxct_err "failed to remove ${execute_file}"
+      has_err=true
+    fi
   else
-    echo_fatal "Can not found script ${SCRIPT_NAME} on path:${execute_file}"
+    yxct_err "Can not found ${SCRIPT_NAME} on path:${execute_file}"
+    has_err=true
+  fi
+
+
+  # remove library
+  local library_path=$(_yxct_get_library_path)
+  if [ -n "${library_path}" ]; then
+    if ! yxct_verbcmd "rm -rf ${library_path}"; then
+      has_err=true
+      yxct_err "failed to remove library:${library_path}"
+    fi
+  else
+    has_err=true
+    yxct_err "failed to get library path"
+  fi
+
+
+  if ${has_err}; then
+    yxct_fatal "failed to uninstall ${SCRIPT_NAME}"
+  else
+    yxct_msg "Unstall success. You need to logout and re-login to reload all environment."
   fi
 
   return 0
-}
 
+}
 
 
 
@@ -163,7 +254,7 @@ function cmd_uninstall()
 sub_cmd=$1
 shift
 if [[ -z ${sub_cmd} ]]; then
-  echo_fatal "Param error. use '${CMD} --help' to show document"
+  yxct_fatal "Param error. use '${CMD} --help' to show document"
 fi
 
 # echo "Here is setup script. sub-cmd:${sub_cmd}"
@@ -174,6 +265,6 @@ if [[ ${sub_cmd} == 'install' ]]; then
 elif [[ ${sub_cmd} == 'uninstall' ]]; then
   cmd_uninstall $@
 else
-  echo_fatal "Unknown sub command:'${sub_cmd}'."
+  yxct_fatal "Unknown sub command:'${sub_cmd}'."
 fi
 
