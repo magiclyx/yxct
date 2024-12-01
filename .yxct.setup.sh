4b2923ceb4328dbe97d7090aff591abe6d9e0af4
Yuxi Liu <liu.yuxi.canaan@gmail.com>
2024-12-01 19:07:02 +0800

    修改安装脚本，使用 yxct 中的实现

--- .yxct.setup.sh
+++ .yxct.setup.sh
@@ -5,28 +5,6 @@
 SCRIPT_NAME=yxct
 
 
-
-####################################################################################################
-# utility
-####################################################################################################
-
-
-function _yxct_is_osx()
-{
-  local _is_osx=false
-
-  if [ -n "${OSTYPE}" ]; then
-    if [[ "${OSTYPE}" == "darwin"* ]]; then 
-      _is_osx=true
-    else
-      _is_osx=false
-    fi
-  else
-    if [[ $(uname | tr '[:upper:]' '[:lower:]') == "darwin"* ]]; then
-      _is_osx=true
-    else
-      _is_osx=false
-    fi
 YXCT_COMMAND_PATH="$( cd -P "$( dirname "$0"  )" && pwd  )/${SCRIPT_NAME}"
 if ! [ -f "${YXCT_COMMAND_PATH}" ]; then
   YXCT_COMMAND_PATH=$(which "${SCRIPT_NAME}")
@@ -40,39 +18,11 @@ if ! [ -f "${YXCT_COMMAND_PATH}" ]; then
   fi
 fi
 
-  ${_is_osx} && return 0 || return 1
-}
-
-
-function yxct_fatal()
-{
-  echo "Fatal: $@"
-  exit 1
-}
-
-function yxct_verb()
-{
-  echo $@
-}
-
-function yxct_err()
-{
-  (>&2 echo "$(tput setaf 5)${1}$(tput sgr0)")
-}
 if [ -n "${YXCT_COMMAND_PATH}" ]; then
   source "${YXCT_COMMAND_PATH}" -
 fi
 
-function yxct_verbcmd()
-{
-  yxct_verb "$*"
-  $@
-}
 
-function yxct_msg()
-{
-  echo $*
-}
 
 ####################################################################################################
 # command
@@ -134,32 +84,32 @@ function cmd_install()
   # remove old libpath if exist
   local cmd_lib_path="${lib_path%\/}/${command}"
   if [ -d "${cmd_lib_path}" ]; then
-    if ! yxct_verbcmd "rm -rf ${cmd_lib_path}"; then
+    if ! yxct_verbcmd "${RM} -rf ${cmd_lib_path}"; then
       yxct_fatal "failed to remove old lib"
     fi
   fi
 
 
   # create lib dir
-  yxct_verbcmd "mkdir -p ${cmd_lib_path}"
+  yxct_verbcmd "${MKDIR} -p ${cmd_lib_path}"
   if ! [ -d "${cmd_lib_path}" ]; then
     yxct_fatal "Failed to make dir:${cmd_lib_path}"
   fi
 
 
   # copy whole dir to lib
-  yxct_verbcmd "cp -R ${PWD}/. ${cmd_lib_path}"
+  yxct_verbcmd "${CP} -R ${PWD}/. ${cmd_lib_path}"
   if [[ $? != 0 ]]; then
-    yxct_fatal "failed to cp yxlib to ${cmd_lib_path}"
+    yxct_fatal "failed to copy yxlib to ${cmd_lib_path}"
   fi
-  yxct_verbcmd "chmod -R 755 ${cmd_lib_path}"
+  yxct_verbcmd "${CHMOD} -R 755 ${cmd_lib_path}"
   if [[ $? != 0 ]]; then
     yxct_fatal "faild to change yxlib's authorization"
   fi
 
 
   # link command to bin path
-  if ! yxct_verbcmd "ln -sf ${cmd_lib_path}/yxct ${bin_path%/}/${command}"; then
+  if ! yxct_verbcmd "${LN} -sf ${cmd_lib_path}/yxct ${bin_path%/}/${command}"; then
     yxct_fatal "faild to link command to ${bin_path}"
   fi
 
@@ -186,11 +136,11 @@ function cmd_install()
     if [ -n "${install_script}" ]; then
       local cellar_path="${cmd_lib_path}/cellar/${searched_command}"
 
-      if ! yxct_verbcmd "mkdir -p ${cellar_path}"; then
+      if ! yxct_verbcmd "${MKDIR} -p ${cellar_path}"; then
         yxct_fatal "failed to make DIR:${cellar_path}"
       fi
 
-      yxct_verbcmd "cp ${install_script} ${cellar_path}"
+      yxct_verbcmd "${CP} ${install_script} ${cellar_path}"
     fi
 
 
@@ -269,7 +219,7 @@ function cmd_uninstall()
   local execute_file="${bin_path}/${command}"
   if [ -x "${execute_file}" ]; then
     
-    if ! yxct_verbcmd "rm -f ${execute_file}"; then
+    if ! yxct_verbcmd "${RM} -f ${execute_file}"; then
       yxct_err "failed to remove ${execute_file}"
       has_err=true
     fi
@@ -281,7 +231,7 @@ function cmd_uninstall()
 
   # remove library
   local cmd_lib_path="${lib_path%\/}/${command}"
-  if ! yxct_verbcmd "rm -rf ${cmd_lib_path}"; then
+  if ! yxct_verbcmd "${RM} -rf ${cmd_lib_path}"; then
     has_err=true
     yxct_err "failed to remove library:${cmd_lib_path}"
   fi
